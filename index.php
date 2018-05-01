0271234e0f3b635199c80a29de47c0663b6f741a
Redq <redq_000@mail.ru>
2018-05-01 09:18:12 +0500

    Добавил массивы и цикл

--- index.php
+++ index.php
@@ -53,6 +53,46 @@ $user_avatar = 'img/user.jpg';
 </header>
 
 <main class="container">
+<?php
+$table = [
+	[
+	    'name' => '2014 Rossignol District Snowboard',
+		'category' => 'Доски и лыжи',
+		'price' => '10999',
+		'url' => 'img/lot-1.jpg',
+	],
+	[
+		'name' => 'DC Ply Mens 2016/2017 Snowboard',
+		'category' => 'Доски и лыжи',
+		'price' => '159999',
+		'url' => 'img/lot-2.jpg',
+	],
+	[
+		'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+		'category' => 'Крепления',
+		'price' => '8000',
+		'url' => 'img/lot-3.jpg',
+	],
+    [
+		'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+		'category' => 'Ботинки',
+		'price' => '10999',
+		'url' => 'img/lot-4.jpg'
+    ],
+    [
+		'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+		'category' => 'Одежда',
+		'price' => '7500',
+		'url' => 'img/lot-5.jpg',
+    ],
+    [
+		'name' => 'Маска Oakley Canopy',
+		'category' => 'Разное',
+		'price' => '5400',
+		'url' => 'img/lot-6.jpg',
+    ]
+];
+?>
     <section class="promo">
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
@@ -82,17 +122,18 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+		    <?php foreach ($table as $key => $item): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?=$item['url']; ?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$item['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$item['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$item['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -100,31 +141,41 @@ $user_avatar = 'img/user.jpg';
                     </div>
                 </div>
             </li>
+			<?php endforeach; ?>
         </ul>
     </section>
 </main>
 
 <footer class="main-footer">
+<?php
+$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+	
+$index = 0;
+$num_count = count($categories);
+?>
     <nav class="nav">
         <ul class="nav__list container">
+		<?php while ($index<$num_count):?>
             <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
+                <a href="all-lots.html"><?=$categories[$index];?></a>
             </li>
             <li class="nav__item">
-                <a href="all-lots.html">Крепления</a>
+                <a href="all-lots.html"></a>
             </li>
             <li class="nav__item">
-                <a href="all-lots.html">Ботинки</a>
+                <a href="all-lots.html"></a>
             </li>
             <li class="nav__item">
-                <a href="all-lots.html">Одежда</a>
+                <a href="all-lots.html"></a>
             </li>
             <li class="nav__item">
-                <a href="all-lots.html">Инструменты</a>
+                <a href="all-lots.html"></a>
             </li>
             <li class="nav__item">
-                <a href="all-lots.html">Разное</a>
+                <a href="all-lots.html"></a>
             </li>
+	    <?php $index = $index + 1; ?>
+	    <?php endwhile; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
