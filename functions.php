42aa63e87d6c6e970092c11493bfaa8c22cd2832
Redq <redq_000@mail.ru>
2018-05-06 04:17:32 +0500

    module3-task1 fix

--- functions.php
+++ functions.php
@@ -1,24 +1,23 @@
 ﻿<?php
 function price_decor($price) {  
- $text = ceil($price);
+    $text = ceil($price);
 
-  if ($price > 1000) {
-	  $price = number_format($price, 0, ',', ' ');	  
-  } 
+    if ($price > 1000) {
+	    $price = number_format($price, 0, ',', ' ');	  
+    } 
   
-  $price .= " Р";
-  return $price;
+    $price .= " Р";
+    return $price;
 }
 
 function include_template($tamplate, $data) {
 	if (file_exists($tamplate)) {
-	ob_start();
-	extract($data);
-	include($tamplate);
-	return ob_get_clean();
-	}
-	else {
-		echo '  ';
+	    ob_start();
+	    extract($data);
+	    include($tamplate);
+	    return ob_get_clean();
+	} else {
+		echo '';
 	}
 }
 ?>
