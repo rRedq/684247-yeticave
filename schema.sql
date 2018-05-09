ec4c0882735a1a4ce8a8c8e81628fbe5616500e5
Redq <redq_000@mail.ru>
2018-05-09 14:51:41 +0500

    fix m4t1

--- schema.sql
+++ schema.sql
@@ -2,14 +2,16 @@ create table lot (
     lot_id int auto_increment primary key ,
     date_start datetime,
     lot_name char(128),
-    description_lot char(256),
+    description_lot char(255),
     image char(128),
     start_price int,
     date_end date,
     step_bet int,
     categories_id int,
     user_id int,
+    user_id int,
     foreign key(categories_id) references categories(categories_id),
+    foreign key(user_id) references user(user_id),
     foreign key(user_id) references user(user_id)
 
 );
@@ -31,16 +33,16 @@ create table user (
     date_registration datetime,
     email char(64),
     name char(64),
-    password int,
+    password char(64),
     avatar char(128),
-    contacts char(256),
-    lot_id int,
-    rate_id int,
-    foreign key(lot_id) references lot(lot_id),
-    foreign key(rate_id) references rate(rate_id)
+    contacts char(255)
 );
 create unique index ui_categories_name on categories(categories_name);
 create unique index ui_user_email on user(email);
 create unique index ui_user_name on user(name);
-create index i_lot_name on lot(lot_name);
-create index i_lot_img on lot(image);
+create fulltext index i_lot_name on lot(lot_name);
+create index i_lot_categories on lot(categories_id);
+create index lot_win on lot(user_id, date_end);
+create fulltext index i_lot_description on lot(description_lot);
+create fulltext index i_lot_name on lot(lot_name);
+create index i_user_email on user(email);
