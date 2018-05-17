1c1f11db92c99f5ecca0884af752bc6527c17ce3
Redq <redq_000@mail.ru>
2018-05-18 01:23:06 +0500

    m6t1_1

--- templates/lot.php
+++ templates/lot.php
@@ -4,7 +4,7 @@
     <div class="lot-item__content">
         <div class="lot-item__left">
             <div class="lot-item__image">
-                <img src="img/lot-image.jpg" width="730" height="548" alt="Сноуборд">
+                <img src="<?=$value['image'];?>" width="730" height="548" alt="Сноуборд">
             </div>
             <p class="lot-item__category">Категория: <span><?=$value['categories_name'];?></span></p>
             <p class="lot-item__description"><?=$value['lot_description'];?></p>
