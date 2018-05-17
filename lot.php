1a610c10cd5eedfe23b2e3526f89a9f2f41c77d1
Redq <redq_000@mail.ru>
2018-05-18 01:19:41 +0500

    m6t1

--- lot.php
+++ lot.php
@@ -4,15 +4,29 @@ date_default_timezone_set('Europe/Moscow');
 require_once("functions.php");
 
 $link = mysqli_connect('localhost', 'root', '', 'schema')
-    or die ('Ошибка ' . mysqli_error($link));
+or die ('Ошибка ' . mysqli_error($link));
+
 $sql = 'SELECT categories_id, categories_name, css_class FROM categories';
 $result = mysqli_query($link, $sql);
 
 $sql_table = "SELECT * FROM lot as c left join categories as u on c.categories_id = u.categories_id";
 $result_table = mysqli_query($link, $sql_table);
 
+if (!isset ($_GET['id'])){
+    exit('404');
+}
+
+$id = $_GET['id'];
+
+$sql_lot = "SELECT * FROM lot as c LEFT JOIN categories AS u ON c.categories_id = u.categories_id
+    WHERE lot_id = ?";
+$stmt = mysqli_prepare($link, $sql_lot);
+mysqli_stmt_bind_param($stmt, 'i',$id);
+mysqli_stmt_execute($stmt);
+$result_lot = mysqli_stmt_get_result($stmt);
 mysqli_close($link);
 
+$lot = mysqli_fetch_all($result_lot, MYSQLI_ASSOC);
 $table = mysqli_fetch_all($result_table, MYSQLI_ASSOC);
 $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
@@ -21,13 +35,13 @@ $is_auth = (bool) rand(0, 1);
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
 
-$page_content = include_template('templates/index.php', [
-    'table'=> $table,
-    'categories' => $categories]);
+$lot_content = include_template ('templates/lot.php', [
+    'lot'=> $lot
+]);
 $layout_content = include_template('templates/layout.php',  [
-    'content' => $page_content,
+    'content' => $lot_content,
     'categories' => $categories,
-    'title' => 'Главная страница',
+    'title' => 'Лот',
     'is_auth' => $is_auth,
     'user_name' => $user_name,
     'user_avatar' => $user_avatar
--- templates/index.php
+++ templates/index.php
@@ -21,7 +21,7 @@
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?=$item['categories_name']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=htmlspecialchars($item['lot_name']); ?></a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="lot.php?id=<?=$item['lot_id'];?>"><?=htmlspecialchars($item['lot_name']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
